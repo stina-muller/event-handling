ed4bc2574f98e64c540bac7a337bf386d0a2c185
Kristina Muller <Stina@Kristinas-MacBook-Pro-2.local>
2017-12-16 04:09:04 +1300

    added 3 and 4

--- events.js
+++ events.js
@@ -8,6 +8,8 @@ function start () {
   // when you mouse over it.
   one()
   two()
+  three ()
+  four ()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -29,19 +31,31 @@ function two () {
   two.addEventListener('mouseleave', makeWhite)
 }
 
-// CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.addEventListener('mouseenter', makeOrange)
+  three.addEventListener('mouseleave', makeWhite)
+}
 
-// CREATE FUNCTION four HERE
+function four () {
+  var four = document.getElementById('four')
+  four.addEventListener('click', makePurple)
+  four.addEventListener('click', makeWhite)
+}
 
 // Changes the background color of event's target
 function makeBlue (evt) {
   evt.target.style.backgroundColor = 'blue'
 }
-
 function makeGreen (evt) {
   evt.target.style.backgroundColor = 'green'
 }
-
+function makeOrange (evt) {
+  evt.target.style.backgroundColor = 'orange'
+}
+function makePurple (evt) {
+  evt.target.style.backgroundColor = 'purple'
+}
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
 }
